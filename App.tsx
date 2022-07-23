9d66d0b3e1847f47c767eb7d8adabe70b341038b
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-07-23 09:30:28 -0300

    font and status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import {
   useFonts,
   Roboto_400Regular,
@@ -6,13 +6,20 @@ import {
 } from '@expo-google-fonts/roboto';
 import { THEME } from './src/screens/styles/theme';
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
-  
+  const [fontsLoaded] = useFonts({Roboto_400Regular,
+    Roboto_700Bold});
 
   return (
-    <NativeBaseProvider theme={THEME}>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>  
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />  
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
 
   );
