80b3a5e882191610299cc7c924070b0ecd479508
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-07-25 14:15:39 -0300

    Register Interface

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,14 +16,15 @@ export function Header({ title, ...rest}: Props) {
       alignItems='center'
       bg='gray.600'
       pb={6}
-      pt={12}      
+      pt={12}
+      {...rest}      
     >
       <IconButton 
         icon={<CaretLeft color={colors.gray[200]} size={24}/>}
       />
 
-      <Heading>
-
+      <Heading color='gray.100' textAlign='center' fontSize='lg' flex={1} ml={-6}>
+        {title}
       </Heading>
     </HStack>
   );
